caaa2292f27368a30d5001b9ba1164505e7eb232
Vinicius Oliveira <vr.sena@gmail.com>
2020-11-02 13:30:33 -0500

    Made code for Item SO

---
+++ Assets/Scripts/ItemSO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "myItem", menuName = "Item/ItemSO", order = 1)]
+public class ItemSO : ScriptableObject {
+
+    public string itemName;
+    public Sprite sprite;
+
+}
