bffba0b9724e16be76aaa29c98c361be1f8fc700
Vinicius Oliveira <vr.sena@gmail.com>
2020-11-02 14:43:46 -0500

    Added the information about the dead people

--- Assets/Scripts/ItemSO.cs
+++ Assets/Scripts/ItemSO.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "myItem", menuName = "Item/ItemSO", order = 1)]
-public class ItemSO : ScriptableObject {
+[CreateAssetMenu(fileName = "myItem", menuName = "ScriptableObjects/ItemSO", order = 1)]
+public class ItemSO : ScriptableObject, System.IEquatable<ItemSO> {
 
     public string itemName;
     public Sprite sprite;
+
+    #region EQUALITY_OVERLOAD
+    public override bool Equals(object other) {
+        return Equals(other as ItemSO);
+    }
+
+    public bool Equals(ItemSO itemSO) {
+        if (!itemSO) { return false; }
+
+        return itemName == itemSO.itemName;
+    }
+
+    public override int GetHashCode() {
+        return base.GetHashCode();
+    }
+
+    #endregion
 
 }
---
+++ Assets/Scripts/Tombstone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tombstone : MonoBehaviour {
+
+    public TombstoneSO tombstoneSO;
+    public UnityEngine.UI.Text txtTombstone;
+    public UnityEngine.UI.Text txtNotebook;
+
+    // Start is called before the first frame update
+    void Start() {
+        txtTombstone.text = tombstoneSO.personName + "\n" + tombstoneSO.dates + "\n\n" + tombstoneSO.quotes + "\n\n" + tombstoneSO.hereLies;
+        txtNotebook.text = tombstoneSO.notebook;
+    }
+
+}
