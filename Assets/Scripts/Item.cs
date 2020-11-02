3c021425bf6ac718f7d6da7b88e442796a0ef56b
Vinicius Oliveira <vr.sena@gmail.com>
2020-11-02 13:49:25 -0500

    Made prototype of the item holder

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+    public ItemSO itemSO;
+    public UnityEngine.UI.Image image;
+    string itemName;
+
+    private void Start() {
+        itemName = itemSO.itemName;
+        image.sprite = itemSO.sprite;
+    }
+
+}
