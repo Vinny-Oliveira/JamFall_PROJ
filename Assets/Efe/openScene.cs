575866cb2543d444ae3141530c1145369ca88bec
EfeKaracar <efekaracar@gmail.com>
2020-11-04 13:41:18 -0500

    Master art
    Master art

---
+++ Assets/Efe/openScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class openScene : MonoBehaviour
+{
+    public void loadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
